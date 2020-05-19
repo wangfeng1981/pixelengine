20ba1a17b0d89289e45054c0aff8220a777238ce
wangfeng_dq <wangfeng_dq@piesat.cn>
2020-05-19 19:27:22 +0800

    Dataset.RenderPsuedColor FillRange

--- pe_tilecompute/testPEApi/testapi.cpp
+++ pe_tilecompute/testPEApi/testapi.cpp
@@ -13,6 +13,7 @@
 #include <ctime>
 #include <chrono>
 #include "lodepng.h"
+#include  <algorithm>
 
 using namespace v8;
 using namespace std;
@@ -22,11 +23,148 @@ using namespace std;
 Global<ObjectTemplate> PEDatasetTemplate ;
 Global<Value> PEDatasetForEachPixelFunction;
 
+//Dataset methods:
 Global<Value> GlobalFunc_ForEachPixelCallBack ;
 extern void GlobalFunc_RenderGrayCallBack(const v8::FunctionCallbackInfo<v8::Value>& args) ;
+extern void GlobalFunc_RenderPsuedColorCallBack(const v8::FunctionCallbackInfo<v8::Value>& args) ;
+extern void GlobalFunc_FillRangeCallBack(const v8::FunctionCallbackInfo<v8::Value>& args) ;
+
+//global methods:
 extern void GlobalFunc_Log(const v8::FunctionCallbackInfo<v8::Value>& args) ;
 //extern void GlobalFunc_ForEachPixelCallBack(const v8::FunctionCallbackInfo<v8::Value>& args) ;//in JavaScript
- 
+
+
+struct PixelEngine
+{
+	static vector<int> ColorRainbow  ;//1
+	static vector<int> ColorBlues  ;//2
+	static vector<int> ColorReds  ;//3
+	static vector<int> ColorGreens  ;//4
+	static vector<int> ColorGrays  ;//0 and others
+
+	static vector<int> GetColorRamp(int colorid,int inverse=0) ;
+	static void Value2Color(int valx,float K,int nodata,int* nodataColor,int vmin,int interpol,vector<int>& colorRamp,int ncolor,unsigned char& rr,unsigned char& rg,unsigned char& rb,unsigned char& ra );
+} ;
+
+vector<int> PixelEngine::GetColorRamp(int colorid,int inverse) 
+{
+	if( colorid==1 ){
+		if( inverse==0 )
+		{
+			return PixelEngine::ColorRainbow ;
+		}else
+		{
+			vector<int> v = PixelEngine::ColorRainbow ;
+    		std::reverse(std::begin(v), std::end(v));
+    		return v ;
+		}
+	}else if( colorid==2 )
+	{
+		if( inverse==0 )
+		{
+			return PixelEngine::ColorBlues ;
+		}else
+		{
+			vector<int> v = PixelEngine::ColorBlues ;
+    		std::reverse(std::begin(v), std::end(v));
+    		return v ;
+		}
+	}else if( colorid==3 ){
+		if( inverse==0 )
+		{
+			return PixelEngine::ColorReds ;
+		}else
+		{
+			vector<int> v = PixelEngine::ColorReds ;
+    		std::reverse(std::begin(v), std::end(v));
+    		return v ;
+		}
+	}else if( colorid==4 ){
+		if( inverse==0 )
+		{
+			return PixelEngine::ColorGreens ;
+		}else
+		{
+			vector<int> v = PixelEngine::ColorGreens ;
+    		std::reverse(std::begin(v), std::end(v));
+    		return v ;
+		}
+	}else
+	{
+		if( inverse==0 )
+		{
+			return PixelEngine::ColorGrays ;
+		}else
+		{
+			vector<int> v = PixelEngine::ColorGrays ;
+			std::reverse(std::begin(v), std::end(v));
+			return v ;
+		}
+	}
+}
+
+vector<int> PixelEngine::ColorRainbow{
+215,25,28
+,232,91,58
+,249,158,89
+,254,201,128
+,255,237,170
+,237,248,185
+,199,233,173
+,157,211,167
+,100,171,176
+,43,131,186
+} ;
+vector<int> PixelEngine::ColorBlues{
+ 247,251,255
+,226,238,249
+,205,224,242
+,176,210,232
+,137,191,221
+,96,166,210
+,62,142,196
+,33,114,182
+,10,84,158
+,8,48,107
+} ;
+vector<int> PixelEngine::ColorReds{
+255, 245, 240
+,254, 227, 214
+,253, 198, 175
+,252, 164, 134
+,252,146,114
+,251,106,74
+,239,59,44
+,203,24,29
+,165,15,21
+,103,0,13
+} ;
+vector<int> PixelEngine::ColorGreens{
+	 247,252,245
+,232,246,227
+,208,237,202
+,178,224,171
+,142,209,140
+,102,189,111
+,61,167,90
+,35,140,69
+,3,112,46
+,0,68,27
+} ;
+vector<int> PixelEngine::ColorGrays{
+250,250,250
+,223,223,223
+,196,196,196
+,168,168,168
+,141,141,141
+,114,114,114
+,87,87,87
+,59,59,59
+,32,32,32
+,5,5,5
+
+} ;
+
 
 // global->Set(String::NewFromUtf8Literal(GetIsolate(), "log"),
 //             FunctionTemplate::New(GetIsolate(), LogCallback));
@@ -59,6 +197,14 @@ Local<Object> CPP_NewDataset(Isolate* isolate,Local<Context>& context
 	ds->Set(context
 		,String::NewFromUtf8(isolate, "renderGray").ToLocalChecked(),
             FunctionTemplate::New(isolate, GlobalFunc_RenderGrayCallBack)->GetFunction(context).ToLocalChecked() );
+	ds->Set(context
+		,String::NewFromUtf8(isolate, "renderPsuedColor").ToLocalChecked(),
+            FunctionTemplate::New(isolate, GlobalFunc_RenderPsuedColorCallBack)->GetFunction(context).ToLocalChecked() );
+	ds->Set(context
+		,String::NewFromUtf8(isolate, "fillRange").ToLocalChecked(),
+            FunctionTemplate::New(isolate, GlobalFunc_FillRangeCallBack)->GetFunction(context).ToLocalChecked() );
+
+	
 
 	ds->Set(context
 		,String::NewFromUtf8(isolate, "width").ToLocalChecked()
@@ -218,7 +364,242 @@ void GlobalFunc_RenderGrayCallBack(const v8::FunctionCallbackInfo<v8::Value>& ar
 	
 	//info.GetReturnValue().Set(i16arr);
 	args.GetReturnValue().Set(outds) ;
+}
 
+//fill value in the same dataset , not create a new one , no return.
+//iband,vmin,vmax
+void GlobalFunc_FillRangeCallBack(const v8::FunctionCallbackInfo<v8::Value>& args) 
+{
+	cout<<"inside GlobalFunc_FillRangeCallBack"<<endl; 
+	if (args.Length() != 3 ){
+		cout<<"Error: args.Length !=2 "<<endl ;
+		return;
+	}
+	Isolate* isolate = args.GetIsolate() ;
+	v8::HandleScope handle_scope(isolate);
+	Local<Context> context(isolate->GetCurrentContext()) ;
+
+	Local<Value> v8_iband = args[0];
+	Local<Value> v8_vmin = args[1] ;
+	Local<Value> v8_vmax = args[2] ;
+
+	int iband = v8_iband->ToInteger(context).ToLocalChecked()->Value() ;
+	int vmin = v8_vmin->ToInteger(context).ToLocalChecked()->Value() ;
+	int vmax = v8_vmax->ToInteger(context).ToLocalChecked()->Value() ;
+	
+	Local<Object> thisobj =  args.This() ;
+	int thisDataType = thisobj->Get(context,
+		String::NewFromUtf8(isolate,"dataType").ToLocalChecked())
+		.ToLocalChecked()->ToInteger(context).ToLocalChecked()->Value() ;
+	//cout<<"thisDataType "<<thisDataType<<endl;
+
+	int width = thisobj->Get(context,
+		String::NewFromUtf8(isolate,"width").ToLocalChecked())
+		.ToLocalChecked()->ToInteger(context).ToLocalChecked()->Value() ;
+	//cout<<"thisDataType "<<thisDataType<<endl;
+
+	int height = thisobj->Get(context,
+		String::NewFromUtf8(isolate,"height").ToLocalChecked())
+		.ToLocalChecked()->ToInteger(context).ToLocalChecked()->Value() ;
+	//cout<<"thisDataType "<<thisDataType<<endl;
+
+	int nband = thisobj->Get(context,
+		String::NewFromUtf8(isolate,"nband").ToLocalChecked())
+		.ToLocalChecked()->ToInteger(context).ToLocalChecked()->Value() ;
+
+	Local<Value> tiledataValue = thisobj->Get(context,
+		String::NewFromUtf8(isolate,"tiledata").ToLocalChecked())
+		.ToLocalChecked() ;
+
+	int asize = width * height ;
+	float theK = (vmax-vmin)*1.f/asize ;
+	if( thisDataType==3 )
+	{//short
+		
+		Int16Array* i16Array = Int16Array::Cast(*tiledataValue) ;
+		short* backData = (short*) i16Array->Buffer()->GetBackingStore()->Data() ;
+		short* backDataOffset = backData + iband * asize;
+		for(int it = 0 ; it < asize ; ++ it )
+		{
+			backDataOffset[it] = it*theK + vmin ;
+		}
+	}else
+	{//byte
+		Uint8Array* u8Array = Uint8Array::Cast(*tiledataValue) ;
+		unsigned char* backData = (unsigned char*) u8Array->Buffer()->GetBackingStore()->Data() ;
+		unsigned char* backDataOffset = backData + iband * asize;
+		for(int it = 0 ; it < asize ; ++ it )
+		{
+			backDataOffset[it] = it*theK + vmin ;
+		}
+	}
+	//info.GetReturnValue().Set(i16arr);
+	//args.GetReturnValue().Set(outds) ;
+}
+
+void PixelEngine::Value2Color(int valx,float K
+	,int nodata,int* nodataColor
+	,int vmin , int interpol
+	,vector<int>& colorRamp , int ncolor
+	,unsigned char& rr 
+	,unsigned char& rg 
+	,unsigned char& rb 
+	,unsigned char& ra ){
+	if( valx == nodata ){
+		rr = nodataColor[0] ;
+		rg = nodataColor[1] ;
+		rb = nodataColor[2] ;
+		ra = nodataColor[3] ;
+	}else
+	{
+		int dn = (valx-vmin) * K ;
+		float weightHigh = (valx-vmin) * K - dn ;
+		if( dn < 0 )
+		{
+			rr = colorRamp[0] ;
+			rg =  colorRamp[1] ;
+			rb =  colorRamp[2]  ;
+			ra = 255;
+		}
+		else if( dn >= ncolor-1 ) 
+		{
+			int off = (ncolor-1)*3;
+			rr =  colorRamp[off] ; 
+			rg =  colorRamp[off+1] ;
+			rb =  colorRamp[off+2] ; 
+			ra = 255;
+
+		}else
+		{
+			int off0 = dn*3 ;
+			int off1 = off0+3 ;
+			if( interpol ==1 )
+			{//interpol
+				float wl = 1.f-weightHigh ;
+				rr = colorRamp[off0]*wl+colorRamp[off1]*weightHigh ;
+				rg = colorRamp[off0+1]*wl+colorRamp[off1+1]*weightHigh ;
+				rb = colorRamp[off0+2]*wl+colorRamp[off1+2]*weightHigh ; 
+				ra = 255;
+			}else
+			{//discrete
+				rr = colorRamp[off0] ;
+				rg = colorRamp[off0+1];
+				rb = colorRamp[off0+2] ;
+				ra = 255;
+			}
+		}
+	}
+}
+
+
+//iband,vmin,vmax,nodata,nodataColor,colorid,noraml/inverse(0/1),discrete/interpol(0/1)
+void GlobalFunc_RenderPsuedColorCallBack(const v8::FunctionCallbackInfo<v8::Value>& args) 
+{
+	cout<<"inside GlobalFunc_RenderPsuedColorCallBack"<<endl; 
+	if (args.Length() != 8 ){
+		cout<<"Error: args.Length != 8 "<<endl ;
+		return;
+	}
+	Isolate* isolate = args.GetIsolate() ;
+	v8::HandleScope handle_scope(isolate);
+	Local<Context> context(isolate->GetCurrentContext()) ;
+
+	Local<Value> v8_iband = args[0];
+	Local<Value> v8_vmin = args[1] ;
+	Local<Value> v8_vmax = args[2] ;
+	Local<Value> v8_nodata = args[3] ;
+	Local<Value> v8_nodatacolor = args[4] ;
+	Local<Value> v8_colorid = args[5] ;
+	Local<Value> v8_inverse = args[6] ;
+	Local<Value> v8_interpol = args[7] ;
+
+	int iband = v8_iband->ToInteger(context).ToLocalChecked()->Value() ;
+	int vmin = v8_vmin->ToInteger(context).ToLocalChecked()->Value() ;
+	int vmax = v8_vmax->ToInteger(context).ToLocalChecked()->Value() ;
+	int nodata = v8_nodata->ToInteger(context).ToLocalChecked()->Value() ;
+	Local<Object> nodataColorObj = v8_nodatacolor->ToObject(context).ToLocalChecked() ;
+	int nodataColor[] = {0,0,0,0} ;
+	nodataColor[0] = nodataColorObj->Get(context,0).ToLocalChecked()->ToInteger(context).ToLocalChecked()->Value() ;
+	nodataColor[1] = nodataColorObj->Get(context,1).ToLocalChecked()->ToInteger(context).ToLocalChecked()->Value() ;
+	nodataColor[2] = nodataColorObj->Get(context,2).ToLocalChecked()->ToInteger(context).ToLocalChecked()->Value() ;
+	nodataColor[3] = nodataColorObj->Get(context,3).ToLocalChecked()->ToInteger(context).ToLocalChecked()->Value() ;
+
+	int colorid = v8_colorid->ToInteger(context).ToLocalChecked()->Value() ;
+	int inverse = v8_inverse->ToInteger(context).ToLocalChecked()->Value() ;
+	int interpol = v8_interpol->ToInteger(context).ToLocalChecked()->Value() ;
+
+	vector<int> colorRamp = PixelEngine::GetColorRamp(colorid,inverse) ;
+	int ncolor = colorRamp.size()/3 ;
+
+	Local<Object> thisobj =  args.This() ;
+	int thisDataType = thisobj->Get(context,
+		String::NewFromUtf8(isolate,"dataType").ToLocalChecked())
+		.ToLocalChecked()->ToInteger(context).ToLocalChecked()->Value() ;
+	//cout<<"thisDataType "<<thisDataType<<endl;
+
+	int width = thisobj->Get(context,
+		String::NewFromUtf8(isolate,"width").ToLocalChecked())
+		.ToLocalChecked()->ToInteger(context).ToLocalChecked()->Value() ;
+	//cout<<"thisDataType "<<thisDataType<<endl;
+
+	int height = thisobj->Get(context,
+		String::NewFromUtf8(isolate,"height").ToLocalChecked())
+		.ToLocalChecked()->ToInteger(context).ToLocalChecked()->Value() ;
+	//cout<<"thisDataType "<<thisDataType<<endl;
+
+	int nband = thisobj->Get(context,
+		String::NewFromUtf8(isolate,"nband").ToLocalChecked())
+		.ToLocalChecked()->ToInteger(context).ToLocalChecked()->Value() ;
+	//cout<<"thisDataType "<<thisDataType<<endl;
+
+	//output
+	Local<Object> outds = CPP_NewDataset(isolate,context
+		,1
+		,width
+		,height
+		,4 );
+	Local<Value> outDataValue = outds->Get(context,
+		String::NewFromUtf8(isolate,"tiledata").ToLocalChecked())
+		.ToLocalChecked() ;
+	Uint8Array* outU8Array = Uint8Array::Cast(*outDataValue) ;
+	unsigned char* outbackData = (unsigned char*) outU8Array->Buffer()->GetBackingStore()->Data() ;
+
+
+	Local<Value> tiledataValue = thisobj->Get(context,
+		String::NewFromUtf8(isolate,"tiledata").ToLocalChecked())
+		.ToLocalChecked() ;
+
+	int asize = width * height ;
+	float theK = (ncolor-1.f)/(vmax-vmin) ;
+	if( thisDataType==3 )
+	{//short
+		
+		Int16Array* i16Array = Int16Array::Cast(*tiledataValue) ;
+		short* backData = (short*) i16Array->Buffer()->GetBackingStore()->Data() ;
+		short* backDataOffset = backData + iband * asize;
+		int tcolor[3] ;
+		for(int it = 0 ; it < asize ; ++ it )
+		{
+			PixelEngine::Value2Color(backDataOffset[it],theK,nodata,nodataColor
+				,vmin,interpol,colorRamp,ncolor,outbackData[it],outbackData[it+asize]
+				,outbackData[it+asize*2],outbackData[it+asize*3]) ;
+
+		}
+	}else
+	{//byte
+		Uint8Array* u8Array = Uint8Array::Cast(*tiledataValue) ;
+		unsigned char* backData = (unsigned char*) u8Array->Buffer()->GetBackingStore()->Data() ;
+		unsigned char* backDataOffset = backData + iband * asize;
+		for(int it = 0 ; it < asize ; ++ it )
+		{
+			PixelEngine::Value2Color(backDataOffset[it],theK,nodata,nodataColor
+				,vmin,interpol,colorRamp,ncolor,outbackData[it],outbackData[it+asize]
+				,outbackData[it+asize*2],outbackData[it+asize*3]) ;
+		}
+	}
+	
+	//info.GetReturnValue().Set(i16arr);
+	args.GetReturnValue().Set(outds) ;
 }
 
 
@@ -293,7 +674,16 @@ void Dataset2Png( Isolate* isolate, Local<Context>& context, Local<Value> dsValu
 				rgbadata[it * 4 + 2] = dataptr[it+2*imgsize];
 				rgbadata[it * 4 + 3] = dataptr[it+3*imgsize];
 			}
-		}else
+		}else if(nband==3 )
+		{
+			for (int it = 0; it < imgsize; ++it)
+			{
+				rgbadata[it * 4 + 0] = dataptr[it];
+				rgbadata[it * 4 + 1] = dataptr[it+imgsize];
+				rgbadata[it * 4 + 2] = dataptr[it+2*imgsize];
+				rgbadata[it * 4 + 3] = 255;
+			}
+		} else
 		{
 			for (int it = 0; it < imgsize; ++it)
 			{
@@ -326,9 +716,35 @@ void initTemplate( Isolate* isolate, Local<Context>& context )
 	Local<Object> global = context->Global() ;
 	//var PixelEngine = {} ;
 	Local<Object> pe = Object::New(isolate) ;
+
 	Maybe<bool> okpe = global->Set( context
 		,String::NewFromUtf8(isolate, "PixelEngine").ToLocalChecked()
 		,pe ) ;// bind PixelEngine
+	pe->Set(context,String::NewFromUtf8(isolate, "ColorRampRainbow").ToLocalChecked(),
+	   Integer::New(isolate,1));
+	pe->Set(context,String::NewFromUtf8(isolate, "ColorRampBlues").ToLocalChecked(),
+	   Integer::New(isolate,2));
+	pe->Set(context,String::NewFromUtf8(isolate, "ColorRampReds").ToLocalChecked(),
+	   Integer::New(isolate,3));
+	pe->Set(context,String::NewFromUtf8(isolate, "ColorRampGreens").ToLocalChecked(),
+	   Integer::New(isolate,4));
+	pe->Set(context,String::NewFromUtf8(isolate, "ColorRampGrays").ToLocalChecked(),
+	   Integer::New(isolate,0));
+	pe->Set(context
+		,String::NewFromUtf8(isolate, "NewDataset").ToLocalChecked(),
+           FunctionTemplate::New(isolate, GlobalFunc_NewDatasetCallBack)->GetFunction(context).ToLocalChecked() );
+
+	//normal/inverse
+	pe->Set(context,String::NewFromUtf8(isolate, "ColorRampNormal").ToLocalChecked(),
+	Integer::New(isolate,0));
+	pe->Set(context,String::NewFromUtf8(isolate, "ColorRampInverse").ToLocalChecked(),
+	Integer::New(isolate,1));
+	//discrete/interpol
+	pe->Set(context,String::NewFromUtf8(isolate, "ColorRampDiscrete").ToLocalChecked(),
+	Integer::New(isolate,0));
+	pe->Set(context,String::NewFromUtf8(isolate, "ColorRampInterpolate").ToLocalChecked(),
+	Integer::New(isolate,1));
+
 
 	//global function log
 	global->Set(context
@@ -436,9 +852,11 @@ int main(int argc, char* argv[]) {
       // Create a string containing the JavaScript source code.
 
     	string source = R"(
-    		var ds0 = globalFunc_newDatasetCallBack(3,256,256,6) ;
+    		var ds0 = PixelEngine.NewDataset(3,256,256,6) ; // globalFunc_newDatasetCallBack(3,256,256,6) ;
     		var ds1 = ds0.forEachPixel(function(pxvals,index){return [pxvals[0]+50,pxvals[1]+100,pxvals[2]+150];}) ;
-    		ds1.renderGray(2,0,255,-1,[0,0,0,0]) ;
+    		//ds1.renderGray(2,0,255,-1,[0,0,0,0]) ;
+    		ds1.fillRange(0,0,1000);
+    		ds1.renderPsuedColor(0,0,1000,-1,[0,0,0,0],PixelEngine.ColorRampGreens,PixelEngine.ColorRampNormal,PixelEngine.ColorRampInterpolate);
     	)";
        
 
